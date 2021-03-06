3bdfe09e922e561c77cb0286dbb447cf6bfde856
Peter Thompson <joebloggsnz@gmail.com>
2021-03-06 18:03:21 +1300

    Adjusted heading sizes so there is more separattion in sizes

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 const moment = require('moment-timezone');
 
-module.exports = (function(eleventyConfig) {
+module.exports = (function (eleventyConfig) {
 
-    eleventyConfig.addFilter("dateFormat", function(dateIn) {
+    eleventyConfig.addFilter("dateFormat", function (dateIn) {
         return moment(dateIn).tz('GMT').format('DD/MM/YYYY');
     });
 
     return {
-        templateFormats: ["njk", "md", "jpg", "jpeg", "png", "html", "css"]
+        templateFormats: ["njk", "md", "jpg", "jpeg", "png", "html", "css", "xml"]
     };
 
 });
