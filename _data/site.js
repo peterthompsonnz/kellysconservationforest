0e178e80d2ce40c2b0a4cb6b009fb83ca9c9e367
PeterT <joebloggsnz@gmail.com>
2021-10-25 22:42:18 +1300

    Added apostrophe to the word kellys

--- _data/site.js
+++ _data/site.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-  siteName: 'Kellys Conservation Forest',  
-  siteDescription: 'Kellys Conservation Forest covers more than 10 hectares of native bush, located at Enner Glynn, Nelson. For over 30 years, the Kelly family, assisted by many community groups, has worked to clear the weeds and replant areas around the original forest, It is now more than four times its original size. They have built trails, picnic areas, steps and bridges, and welcome over 1000 visitors every year through open days, educational visits and school plantings.',
+  siteName: 'Kelly\'s Conservation Forest',  
+  siteDescription: 'Kelly\'s Conservation Forest covers more than 10 hectares of native bush, located at Enner Glynn, Nelson. For over 30 years, the Kelly family, assisted by many community groups, has worked to clear the weeds and replant areas around the original forest, It is now more than four times its original size. They have built trails, picnic areas, steps and bridges, and welcome over 1000 visitors every year through open days, educational visits and school plantings.',
   siteAuthor: 'Peter Thompson',  
   copyrightYear: (new Date()).getFullYear(),  
   url: 'https://kellysconservationforest.netlify.app',
